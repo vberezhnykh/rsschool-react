7c3e042294eb3f41f1dbe8f76a672f968c066ae5
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-03-17 13:15:16 +0300

    feat: add styling to form card

--- src/components/formCards.tsx
+++ src/components/formCards.tsx
@@ -8,26 +8,39 @@ class FormCards extends React.Component<FormState> {
 
   render(): React.ReactNode {
     if (this.props.cards.length === 0) {
-      return <div>Nothing has been sumbitted yet.</div>;
+      return <div className="form-cards--not-found">Nothing has been sumbitted yet.</div>;
     }
     const listItems = this.props.cards.map((card, index) => (
-      <li key={`form-card-${index}`}>
-        <p>Name: {card.name}</p>
-        <p>Surname: {card.surname}</p>
-        <p>Date of Birth: {card.dateOfBirth}</p>
-        <p>Residence: {card.residence}</p>
-        <p>Sex: {card.sex}</p>
-        <p>Profile Picture: </p>
-        <ul>
-          Consent:
-          <li>Name: {card.nameConsent ? '✓' : '✗'}</li>
-          <li>Surname: {card.surnameConsent ? '✓' : '✗'}</li>
-          <li>Date of Birth: {card.dateOfBirthConsent ? '✓' : '✗'}</li>
-          <li>Residence: {card.residenceConsent ? '✓' : '✗'}</li>
-        </ul>
+      <li key={`form-card-${index}`} className="form-cards__item">
+        <div className="card-info-container">
+          <p className="form-cards__name">
+            <span>Name:</span> {card.name}
+          </p>
+          <p>
+            <span>Surname:</span> {card.surname}
+          </p>
+          <p>
+            <span>Date of Birth:</span> {card.dateOfBirth}
+          </p>
+          <p>
+            <span>Residence:</span> {card.residence}
+          </p>
+          <p>
+            <span>Sex:</span> {card.sex}
+          </p>
+          <ul className="consent-list">
+            <span>Consent:</span>
+
+            <li>Name: {card.nameConsent ? '✓' : '✗'}</li>
+            <li>Surname: {card.surnameConsent ? '✓' : '✗'}</li>
+            <li>Date of Birth: {card.dateOfBirthConsent ? '✓' : '✗'}</li>
+            <li>Residence: {card.residenceConsent ? '✓' : '✗'}</li>
+          </ul>
+        </div>
+        <img src="" alt="" className="form-cards__image" />
       </li>
     ));
-    return <ul>{listItems}</ul>;
+    return <ul className="form-cards">{listItems}</ul>;
   }
 }
 
