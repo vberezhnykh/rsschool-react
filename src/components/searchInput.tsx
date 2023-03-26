6057b0e81c168a603e4f960dc49c0c09421fdc98
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-03-26 22:38:33 +0300

    refactor: change SearchInput component to functional component

--- src/components/searchInput.tsx
+++ src/components/searchInput.tsx
@@ -2,25 +2,19 @@ import React from 'react';
 import searchImg from '../assets/search.svg';
 import { SearchInputProps } from '../types';
 
-class SearchInput extends React.Component<SearchInputProps> {
-  constructor(props: SearchInputProps) {
-    super(props);
-  }
-
-  render(): React.ReactNode {
-    return (
-      <div className="search-container">
-        <img src={searchImg} alt="Image of a magnifying glass." className="search__img" />
-        <input
-          type="text"
-          value={this.props.value}
-          onChange={this.props.onChange}
-          className="search__input"
-          placeholder="Type here..."
-        />
-      </div>
-    );
-  }
-}
+const SearchInput: React.FC<SearchInputProps> = ({ value, onChange }) => {
+  return (
+    <div className="search-container">
+      <img src={searchImg} alt="Image of a magnifying glass." className="search__img" />
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        className="search__input"
+        placeholder="Type here..."
+      />
+    </div>
+  );
+};
 
 export default SearchInput;
