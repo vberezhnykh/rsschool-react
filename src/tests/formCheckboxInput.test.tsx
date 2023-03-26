fe1112fc82e4f266fb874953d8f84ce523890d33
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-03-26 12:18:54 +0300

    feat: add test for checkbox form input

---
+++ src/tests/formCheckboxInput.test.tsx
@@ -0,0 +1,21 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import FormCheckboxInput from '../components/formCheckboxInput';
+import React from 'react';
+
+describe('FormCheckBoxInput', () => {
+  it('renders checkbox input', () => {
+    const checkboxAttributes = {
+      inputType: 'checkbox',
+      inputName: 'name-consent',
+      labelText: 'Name',
+      inputRef: React.createRef() as React.RefObject<HTMLInputElement>,
+    };
+    render(<FormCheckboxInput {...checkboxAttributes} />);
+    const checkbox = screen.getByRole('checkbox');
+
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).toHaveAttribute('name', checkboxAttributes.inputName);
+    expect(screen.getByLabelText(`${checkboxAttributes.labelText}:`)).toBeInTheDocument();
+  });
+});
