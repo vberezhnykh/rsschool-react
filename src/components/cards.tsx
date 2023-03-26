8a8a3b18abc156559fb0be80cec388b8201a3527
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-03-26 22:44:18 +0300

    refactor: rewrite Cards component to functional component

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -3,37 +3,32 @@ import MockPosts from '../dummy.json';
 import Card from './card';
 import { CardsProps, Post, Posts } from '../types';
 
-class Cards extends React.Component<CardsProps> {
-  constructor(props: CardsProps) {
-    super(props);
-  }
-
-  getFilteredPosts(posts: Posts) {
+const Cards: React.FC<CardsProps> = ({ value }) => {
+  function getFilteredPosts(posts: Posts) {
     return posts.posts.filter(
       (post) =>
-        post.body.toLowerCase().includes(this.props.value.toLowerCase()) ||
-        post.title.toLowerCase().includes(this.props.value.toLowerCase()) ||
-        post.tags.some((tag) => tag.toLowerCase().includes(this.props.value.toLowerCase()))
+        post.body.toLowerCase().includes(value.toLowerCase()) ||
+        post.title.toLowerCase().includes(value.toLowerCase()) ||
+        post.tags.some((tag) => tag.toLowerCase().includes(value.toLowerCase()))
     );
   }
 
-  getListItems = (filteredPosts: Array<Post>) =>
-    filteredPosts.map((post) => <Card {...post} key={post.id} />);
+  function getListItems(filteredPosts: Array<Post>) {
+    return filteredPosts.map((post) => <Card {...post} key={post.id} />);
+  }
 
-  getCards() {
-    const filteredPosts = this.getFilteredPosts(MockPosts);
+  function getCards() {
+    const filteredPosts = getFilteredPosts(MockPosts);
     return filteredPosts.length === 0 ? (
       <div className="cards__item--not-found">No results were found for your query...</div>
     ) : (
       <ul className="cards" data-testid="cards-list">
-        {this.getListItems(filteredPosts)}
+        {getListItems(filteredPosts)}
       </ul>
     );
   }
 
-  render(): React.ReactNode {
-    return this.getCards();
-  }
-}
+  return getCards();
+};
 
 export default Cards;
