922792776cad6382e015002dcb975972899aa2e8
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-03-26 14:20:53 +0300

    feat: add test for FormPage component

---
+++ src/tests/formPage.test.tsx
@@ -0,0 +1,17 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import FormPage from '../pages/formPage';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('FormPage', () => {
+  it('renders correctly', () => {
+    render(
+      <BrowserRouter>
+        <FormPage />
+      </BrowserRouter>
+    );
+    expect(screen.getByText(/Form Page/i)).toBeInTheDocument();
+    expect(screen.getByText(/Nothing has been sumbitted yet./i)).toBeInTheDocument();
+  });
+});
