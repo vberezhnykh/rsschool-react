0f10b0fcd34908e96661181d1ca2fca423a127c5
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-03-27 15:32:48 +0300

    refactor: rewrite FormSelect to functional component

--- src/components/formSelect.tsx
+++ src/components/formSelect.tsx
@@ -2,31 +2,29 @@ import React from 'react';
 import { FormSelectProps } from '../types';
 import ErrorField from './errorField';
 
-class FormSelect extends React.Component<FormSelectProps> {
-  render(): React.ReactNode {
-    return (
-      <>
-        <label>
-          Residence<span style={{ color: 'red' }}>*</span>:{' '}
-          <select ref={this.props.selectRef} className="form__residence-input" defaultValue="">
-            <option disabled value="">
-              select
-            </option>
-            <option value="Russia">Russia</option>
-            <option value="Ukraine">Ukraine</option>
-            <option value="Belarus">Belarus</option>
-            <option value="Other">Other</option>
-          </select>
-        </label>
-        <ErrorField
-          errorClassName="residence-error"
-          errorMessage="Residence is invalid"
-          errorFieldName="residenceValid"
-          formPageState={this.props.formPageState}
-        />
-      </>
-    );
-  }
-}
+const FormSelect: React.FC<FormSelectProps> = ({ selectRef, formPageState }) => {
+  return (
+    <>
+      <label>
+        Residence<span style={{ color: 'red' }}>*</span>:{' '}
+        <select ref={selectRef} className="form__residence-input" defaultValue="">
+          <option disabled value="">
+            select
+          </option>
+          <option value="Russia">Russia</option>
+          <option value="Ukraine">Ukraine</option>
+          <option value="Belarus">Belarus</option>
+          <option value="Other">Other</option>
+        </select>
+      </label>
+      <ErrorField
+        errorClassName="residence-error"
+        errorMessage="Residence is invalid"
+        errorFieldName="residenceValid"
+        formPageState={formPageState}
+      />
+    </>
+  );
+};
 
 export default FormSelect;
