9a5d41c4bf4ce51c991617d72a42942d2ff48704
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-03-27 15:42:48 +0300

    refactor: rewrite ErrorField component to functional component

--- src/components/errorField.tsx
+++ src/components/errorField.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { ErrorFieldProps } from '../types';
 
-class ErrorField extends React.Component<ErrorFieldProps> {
-  render(): React.ReactNode {
-    return (
-      <span
-        className={`${this.props.errorClassName} ${
-          this.props.formPageState.errorFields.includes(this.props.errorFieldName ?? '')
-            ? `${this.props.errorClassName}--visible`
-            : undefined
-        }`}
-      >
-        {this.props.errorMessage}
-      </span>
-    );
-  }
-}
+const ErrorField: React.FC<ErrorFieldProps> = (props) => {
+  return (
+    <span
+      className={`${props.errorClassName} ${
+        props.formPageState.errorFields.includes(props.errorFieldName ?? '')
+          ? `${props.errorClassName}--visible`
+          : undefined
+      }`}
+    >
+      {props.errorMessage}
+    </span>
+  );
+};
 
 export default ErrorField;
