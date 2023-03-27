e3f2c9bf81066c07f8b5830987f38de47b0fbece
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-03-27 15:35:04 +0300

    refactor: FormCheckboxInput component to functional component

--- src/components/formCheckboxInput.tsx
+++ src/components/formCheckboxInput.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { FormCheckboxInputProps } from '../types';
 
-class FormCheckboxInput extends React.Component<FormCheckboxInputProps> {
-  render(): React.ReactNode {
-    return (
-      <label className="consent-checkbox">
-        {this.props.labelText}:{' '}
-        <input type="checkbox" name={this.props.inputName} ref={this.props.inputRef} />
-      </label>
-    );
-  }
-}
+const FormCheckboxInput: React.FC<FormCheckboxInputProps> = ({
+  labelText,
+  inputName,
+  inputRef,
+}) => {
+  return (
+    <label className="consent-checkbox">
+      {labelText}: <input type="checkbox" name={inputName} ref={inputRef} />
+    </label>
+  );
+};
 
 export default FormCheckboxInput;
