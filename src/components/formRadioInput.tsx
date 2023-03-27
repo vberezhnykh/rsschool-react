089fccf9b925193308ec3266405e277bb9646aea
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-03-27 15:30:41 +0300

    refactor: rewrite FormRadioInput component to functional component

--- src/components/formRadioInput.tsx
+++ src/components/formRadioInput.tsx
@@ -2,27 +2,29 @@ import React from 'react';
 import { FormRadioInputProps } from '../types';
 import ErrorField from './errorField';
 
-class FormRadioInput extends React.Component<FormRadioInputProps> {
-  render(): React.ReactNode {
-    return (
-      <>
-        <div className="sex-container">
-          <span>Male</span>
-          <label className="switcher">
-            <input type="radio" ref={this.props.maleInputRef} name="sex-input" value="Male" />
-            <input type="radio" ref={this.props.femaleInputRef} name="sex-input" value="Female" />
-          </label>
-          <span>Female</span>
-        </div>
-        <ErrorField
-          errorClassName="switcher-error"
-          errorMessage="Switcher is invalid"
-          errorFieldName="sexValid"
-          formPageState={this.props.formPageState}
-        />
-      </>
-    );
-  }
-}
+const FormRadioInput: React.FC<FormRadioInputProps> = ({
+  maleInputRef,
+  femaleInputRef,
+  formPageState,
+}) => {
+  return (
+    <>
+      <div className="sex-container">
+        <span>Male</span>
+        <label className="switcher">
+          <input type="radio" ref={maleInputRef} name="sex-input" value="Male" />
+          <input type="radio" ref={femaleInputRef} name="sex-input" value="Female" />
+        </label>
+        <span>Female</span>
+      </div>
+      <ErrorField
+        errorClassName="switcher-error"
+        errorMessage="Switcher is invalid"
+        errorFieldName="sexValid"
+        formPageState={formPageState}
+      />
+    </>
+  );
+};
 
 export default FormRadioInput;
