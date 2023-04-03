40987bee163ec6a2f003432fdfac2aeb323c8979
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-04-03 21:06:33 +0300

    refactor: change folder structure of test

--- src/tests/pages/formPage.test.tsx
+++ src/tests/pages/formPage.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi } from 'vitest';
-import { cleanup, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import FormPage from '../pages/formPage';
 import { BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
