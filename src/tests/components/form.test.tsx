4c3a8c0df2a0345945b0092f0787dc67576a6fef
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-04-05 21:20:24 +0300

    refactor: change props passed to Form component in form test

--- src/tests/components/form.test.tsx
+++ src/tests/components/form.test.tsx
@@ -5,34 +5,8 @@ import Form from '../../components/form';
 
 describe('Form', () => {
   it('renders component correctly', () => {
-    const formState = {
-      cards: [],
-      formValid: false,
-      nameValid: false,
-      surnameValid: false,
-      dateOfBirthValid: false,
-      residenceValid: false,
-      fileValid: false,
-      sexValid: false,
-      errorFields: [],
-    };
-    const refs = {
-      nameInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      surnameInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      dateInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      residenceInput: React.createRef() as React.RefObject<HTMLSelectElement>,
-      fileInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      maleInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      femaleInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      nameConsentInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      surnameConsentInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      dateOfBirthConsentInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      residenceConsentInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      fileConsentInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      sexConsentInput: React.createRef() as React.RefObject<HTMLInputElement>,
-      form: React.createRef() as React.RefObject<HTMLFormElement>,
-    };
-    render(<Form submitHandler={() => {}} state={formState} refs={refs} />);
+    const formRef = React.createRef<HTMLFormElement>();
+    render(<Form submitHandler={() => {}} formRef={formRef} />);
     expect(screen.getByRole('form')).toBeInTheDocument();
     expect(screen.getByRole('button')).toHaveValue('Submit');
   });
