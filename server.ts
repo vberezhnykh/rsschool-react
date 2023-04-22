829658f624a8849d065d0f619800e041ff0d6a85
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-04-22 19:33:33 +0300

    refactor: remove unnecessary imports

--- server.ts
+++ server.ts
@@ -1,10 +1,5 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import express from 'express';
 import { createServer as createViteServer } from 'vite';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 async function createServer() {
   const app = express();
@@ -17,6 +12,7 @@ async function createServer() {
 
   app.use('*', async (req, res) => {
     const url = req.originalUrl;
+
     try {
       console.log('trying...');
       const { render } = await vite.ssrLoadModule('/src/entry-server.tsx');
