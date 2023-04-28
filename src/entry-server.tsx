86bcc17ac626838fe9505d44e14bd95cb08c05c7
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-04-28 11:20:49 +0300

    feat: change favicon

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -16,7 +16,7 @@ const Html: React.FC<HtmlProps> = ({ app, preloadedState }) => {
     <html>
       <head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/close.svg" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.png" />
         <link rel="stylesheet" href="/src/styles/index.scss" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>RSSchool</title>
